50c9ab1e4ee43c6775e5ec0b9059d9eb5ce79ffb
Abdullah Al Hadi <abdullahalhadi317@gmail.com>
2022-06-26 20:53:31 +0600

    media authentications

--- src/App.js
+++ src/App.js
@@ -1,23 +1,141 @@
-import logo from './logo.svg';
 import './App.css';
+import { getAuth, GoogleAuthProvider, signInWithPopup, FacebookAuthProvider, signOut, GithubAuthProvider, OAuthProvider } from "firebase/auth";
+import initializeAuthentication from './Firebase/firebase.initialize';
+import { useState } from 'react';
+
+initializeAuthentication();
+
+const googleProvider =  new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+const githubProvider = new GithubAuthProvider();
+const yahooProvider = new OAuthProvider('yahoo.com');
+
 
 function App() {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
+  // GoogleSignIn
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+    .then(result =>{
+      const {displayName, email, photoURL} = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL,
+      }
+      setUser(loggedInUser);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }
+
+  // FacebookSignIn
+
+  const handleFacebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+    .then((result) => {
+      const {displayName, email, photoURL} = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL,
+      }
+      setUser(loggedInUser);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }
+
+  // GithubSignIn
+
+  const handleGithubSignIn = () => {
+    signInWithPopup(auth, githubProvider)
+    .then((result) => {
+      const {displayName, email, photoURL} = result.user;
+      const loggedInUser = {
+        name: displayName,
+        email: email,
+        photo: photoURL,
+      }
+      setUser(loggedInUser);
+    }).catch((error) => {
+      console.log(error.message);
+    });
+  }
+
+// YahooSignIn
+
+const handleYahooSignIn = () => {
+  signInWithPopup(auth, yahooProvider)
+  .then((result) => {
+    const {displayName, email, photoURL} = result.user;
+    const loggedInUser = {
+      name: displayName,
+      email: email,
+      photo: photoURL,
+    }
+    setUser(loggedInUser);
+    console.log(result.user)
+  })
+  .catch((error) => {
+    console.log(error.message);
+  });
+}
+
+
+// Sign Out
+
+  const handleSignOut = () => {
+    signOut(auth)
+    .then(() => {
+      setUser({});
+    }).catch((error) => {
+      console.log(error);
+    });
+  }
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        user.accessToken && <div>
+          <h2>Welcome {user.name}</h2>
+          <p>My email address is: {user.email}</p>
+          <img src={user.photo} alt="profilePicture"/>
+        </div>
+      }
+      <br />
+      {
+        !user.accessToken ? (
+      <>
+      <div className='header'>
+        <h1>Welcome To Login Page</h1>
+      </div>
+      <div className='container root-class'>
+        <div className='google'>
+          <button onClick={handleGoogleSignIn}>Sign In with Google</button> 
+        </div>
+        <div className='facebook'>
+          <button onClick={handleFacebookSignIn}>Sign In with Facebook</button>  
+        </div>
+        <div className='github'>
+          <button onClick={handleGithubSignIn}>Sign In with GitHub</button>  
+        </div>
+        <div className='yahoo'>
+          <button onClick={handleYahooSignIn}>Sign In with Yahoo</button>
+        </div>
+
+        <div className='no-account'>
+          <p>No Account? <span>Create an Account</span></p>
+        </div>
+      </div>
+      </>
+        ) :(
+          <button className='signout' onClick={handleSignOut}>Sign out</button>
+      )}
+     
+      
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAYDF3wj7JlgHE6i8PRpN3pgDqZSKTB4Hs",
+    authDomain: "auth-firebase-react-c4525.firebaseapp.com",
+    projectId: "auth-firebase-react-c4525",
+    storageBucket: "auth-firebase-react-c4525.appspot.com",
+    messagingSenderId: "1082740771464",
+    appId: "1:1082740771464:web:05566c89c4cbea7c9e2d3d",
+    measurementId: "G-01NMF638K3"
+  };
+
+export default firebaseConfig;
